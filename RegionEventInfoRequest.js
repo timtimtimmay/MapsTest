1860178fceb01352717f3bbd63d68a025b7446ba
timtimtimmay <trdickinson@gmail.com>
2014-05-01 10:32:26 -0500

    Update and rename RegionEventInfoRequest to RegionEventInfoRequest.js

--- RegionEventInfoRequest.js
+++ RegionEventInfoRequest.js
@@ -1,11 +1,11 @@
 <script>
 /*
-//Pass LatLng for use in request
-//Pass startDate from date on homepage
-//Pass map zoom and use to set radius
-//Send url using http GET
+Pass LatLng for use in request
+Pass startDate from date on homepage
+Pass map zoom and use to set radius
+Send url using http GET
 
-//http://data.pollstar.com/api/pollstar.asmx/RegionEvents?lat=36.7477778&lng=-119.7713889&radius=10&onlyVenuesWithEvents=1&startDate=4/30/2014&dayCount=0&apiKey=23511-8040658
+http://data.pollstar.com/api/pollstar.asmx/RegionEvents?lat=36.7477778&lng=-119.7713889&radius=10&onlyVenuesWithEvents=1&startDate=4/30/2014&dayCount=0&apiKey=23511-8040658
 */
 
 var xmlHttp = null;
@@ -16,7 +16,7 @@ function GetEventInfo(map)
     var Lng = map.lng();
     var Zoom = map.getZoom();
     var Radius = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
-    var StartDate = document.getElementById( "TextBoxCustomerNumber" ).value;
+    var StartDate = "5/1/2014" //document.getElementById( "TextBoxCustomerNumber" ).value;
     
     var Url = "http://data.pollstar.com/api/pollstar.asmx/RegionEvents?lat=" + Lat + "&lng= + Lng + "&radius=" + Radius[19-Zoom] + "&onlyVenuesWithEvents=1&startDate=" + StartDate + "&dayCount=0&apiKey=23511-8040658";
 
