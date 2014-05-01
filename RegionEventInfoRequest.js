e29af9b87a8a18d50fb5f9c15a98d4336b64b4df
timtimtimmay <trdickinson@gmail.com>
2014-05-01 17:20:59 -0500

    Update RegionEventInfoRequest.js

--- RegionEventInfoRequest.js
+++ RegionEventInfoRequest.js
@@ -27,8 +27,30 @@ function GetEventInfo(map)
     xmlHttp.open( "GET", Url, false );
     xmlHttp.send( null );
     return xmlHttp.responseText;
+    
+    if (window.DOMParser)
+    {
+        parser=new DOMParser();
+        xmlDoc=parser.parseFromString(txt,"text/xml");
+    }
+    else // Internet Explorer
+    {
+        xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
+        xmlDoc.async=false;
+        xmlDoc.loadXML(txt); 
+    }
 }
 
+/*
+Loop through XML text and output values
+*/
+for (i=0;i<x.length;i++)
+  {
+  document.write(x[i].nodeName);
+  document.write(": ");
+  document.write(x[i].childNodes[0].nodeValue);
+  document.write("<br>");
+  }
 /*
 function ProcessRequest() 
 {
