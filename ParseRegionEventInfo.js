aa4f054b0fcabbd917ae2de39943337759f86fe8
timtimtimmay <trdickinson@gmail.com>
2014-05-01 20:25:03 -0500

    Create ParseRegionEventInfo.js

---
+++ ParseRegionEventInfo.js
@@ -0,0 +1,24 @@
+    
+    if (window.DOMParser)
+    {
+        parser=new DOMParser();
+        xmlDoc=parser.parseFromString(txt,"text/xml");
+    }
+    else // Internet Explorer
+    {
+        xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
+        xmlDoc.async=false;
+        xmlDoc.loadXML(txt); 
+    }
+    
+    
+/*
+Loop through XML text and output values
+*/
+    for (i=0;i<x.length;i++)
+    {
+        document.write(x[i].nodeName);
+        document.write(": ");
+        document.write(x[i].childNodes[0].nodeValue);
+        document.write("<br>");
+    }
