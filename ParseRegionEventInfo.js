57f783ac9897a1792ef3e920e0ea3729d92e36cb
timtimtimmay <trdickinson@gmail.com>
2014-05-01 21:04:11 -0500

    Update ParseRegionEventInfo.js

--- ParseRegionEventInfo.js
+++ ParseRegionEventInfo.js
@@ -1,23 +1,30 @@
 <script>
 function parseXMLResponse(){
     
+    //The block below is used to generate an XML message from Pollstar for testing
+    var Url = "http://data.pollstar.com/api/pollstar.asmx/RegionEvents?lat=38.6272&lng=-90.1978&radius=8&onlyVenuesWithEvents=1&startDate=5/1/2014&dayCount=0&apiKey=23511-8040658";
+    var xmlResponse = null;
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", Url, false );
+    xmlHttp.send( null );
+    xmlResponse = xmlHttp.responseText;
+    
     if (window.DOMParser)
     {
         parser=new DOMParser();
-        xmlDoc=parser.parseFromString(txt,"text/xml");
+        xmlDoc=parser.parseFromString(xmlResponse,"text/xml");
     }
     else // Internet Explorer
     {
         xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
         xmlDoc.async=false;
-        xmlDoc.loadXML(txt); 
+        xmlDoc.loadXML(xmlResponse); 
     }
     
-    
-/*
-Loop through XML text and output values
-*/
-    for (i=0;i<x.length;i++)
+    /*
+    Loop through XML text and output values
+    */
+    for (i=0;i<xmlDoc.length;i++)
     {
         document.write(x[i].nodeName);
         document.write(": ");
